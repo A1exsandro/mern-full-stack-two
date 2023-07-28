f965a333bc79e9fbde291233161c47df7ff286f4
A1exsandro <alexsandro.acp@gmail.com>
2023-07-28 13:21:38 -0400

    feat: add axios

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+interface Profile {
+  token: string;
+}
+
+const API = axios.create({ baseURL: 'http://localhost:5001'})
+
+API.interceptors.request.use((req) => {
+  const profileString = localStorage.getItem('profile')
+  if (profileString !== null) {
+    const profile: Profile = JSON.parse(profileString)
+    req.headers.Authorization = `Bearer ${profile.token}`
+  }
+
+  return req;
+})
+
+export const signIn = (formData: any) => API.post('/user/signin', formData)
+export const signUp = (formData: any) => API.post('/user/signup', formData)

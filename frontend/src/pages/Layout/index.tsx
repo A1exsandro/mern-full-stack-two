847ab5f21d0eddc188aa3c0d5b48ed52cc068563
A1exsandro <alexsandro.acp@gmail.com>
2023-07-30 12:56:29 -0400

    feat: check if  user exists

--- frontend/src/pages/Layout/index.tsx
+++ frontend/src/pages/Layout/index.tsx
@@ -1,6 +1,39 @@
-import { Outlet, Link } from "react-router-dom" 
+import { useEffect, useState } from "react"
+import { Outlet, Link, useLocation, useNavigate } from "react-router-dom" 
+
+interface UserData {
+  user: {
+    token: string;
+  }
+}
 
 const Layout = () => {
+  const profileString = localStorage.getItem('userData')
+  const initialProfile: UserData = profileString ? 
+    JSON.parse(profileString) : {user: { token: null }}
+  console.log('here',initialProfile.user);
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const initialToken = initialProfile.user.token
+  const [profileToken, setProfileToken] = useState(initialToken)
+
+  const logout = () => {
+    localStorage.clear()
+    navigate('/auth')
+    setProfileToken(initialToken)
+  }
+
+  useEffect(() => {
+    const updatedProfileString = localStorage.getItem('userData')
+    const updatedProfile: UserData = updatedProfileString ? 
+      JSON.parse(updatedProfileString) : { user: { token: "" } }
+    const updatedToken = updatedProfile.user.token;
+    setProfileToken(updatedToken)
+    
+    // TREAT AN EXPIRATION TIME FOR THE TOKEN
+  }, [location]) 
+
   return (
     <>
       <nav 
@@ -10,11 +43,25 @@ const Layout = () => {
         <div className="text-2xl text-blue-500">
           <Link to="/">Memories</Link>
         </div>
-        <button 
-          className="bg-blue-500 text-white px-5 py-2 rounded-xl my-2"
-        >
-          <Link to="/auth">Sing In</Link>
-        </button>
+        {
+          profileToken ? (
+            // LOGOUT
+            <button 
+              className="bg-blue-500 text-white px-5 py-2 rounded-xl my-2"
+              onClick={logout}
+            >
+              Logout
+            </button>
+
+          ) : (
+            // LOGIN
+            <button 
+              className="bg-blue-500 text-white px-5 py-2 rounded-xl my-2"
+            >
+              <Link to="/auth">Sing In</Link>
+            </button>
+          )
+        }
       </nav>
 
       <Outlet />

74e8aa37d65f024b85c5f240fc19b49eabfb6814
A1exsandro <alexsandro.acp@gmail.com>
2023-07-31 15:53:38 -0400

    feat: add createPost form in home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react"
+import Form from "../components/Form"
 import Posts from "../components/Posts"
+import { useDispatch } from 'react-redux'
+import { getPosts, PostMessage } from '../features/postsSlice'
+import { ThunkDispatch } from "@reduxjs/toolkit"
 
 const Home = () => {
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>()
+  const [currentId, setCurrentId] = useState(null)
+
+  useEffect(() => {
+    dispatch(getPosts({} as PostMessage))
+  },[currentId, dispatch])
+
   return (
     <div className="flex justify-center flex-wrap">
       <Posts />
+      <Form currentId={currentId} setCurrentId={setCurrentId} />
     </div>
   )
 }
